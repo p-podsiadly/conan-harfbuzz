f27cebf25d2216c799ce889b654f65a35057af68
Uilian Ries <uilianries@gmail.com>
2019-09-30 19:25:07 -0300

    Use freetype from ConanCenterIndex

--- conanfile.py
+++ conanfile.py
@@ -43,7 +43,7 @@ class HarfbuzzConan(ConanFile):
 
     def requirements(self):
         if self.options.with_freetype:
-            self.requires.add("freetype/2.10.0@bincrafters/stable")
+            self.requires.add("freetype/2.10.0")
         if self.options.with_icu:
             self.requires.add("icu/64.2@bincrafters/stable")
         if self.options.with_glib:
