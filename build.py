9848883d70b4eb32abe49d4584b54c8c4e6a839d
Piotr Podsiadly <ppodsiadly@mykolab.com>
2020-01-27 15:32:48 +0100

    Disable glib integration by default, additional builds in CI

--- build.py
+++ build.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
+import platform
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
 
     builder = build_template_default.get_builder()
+    builder.run()
 
+    builder = build_template_default.get_builder(options=["with_glib=True"])
+    builder.run()
+
+    builder = build_template_default.get_builder(options=["with_icu=True"])
     builder.run()
